8ef61acf1eb4ea0aeac49bac03254958a5520707
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-22 12:09:18 -0500

    added ability to feed pets, pet form

--- pets/models.py
+++ pets/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Pet(models.Model):
@@ -9,3 +18,24 @@ class Pet(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('details', kwargs={'pet_id' : self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count()>=len(MEALS)
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length = 1,
+        choices = MEALS,
+        default = MEALS[0][0]
+    )
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- pets/views.py
+++ pets/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Pet
+from .forms import FeedingForm
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -13,4 +17,27 @@ def pets_index(request):
 
 def pet_details(request, pet_id):
     pet = Pet.objects.get(id=pet_id)
-    return render(request, 'pets/details.html', {'pet': pet})
+    form = FeedingForm()
+    return render(request, 'pets/details.html', {'pet': pet, 'feeding_form': form})
+
+def add_feeding(request, pet_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.pet_id = pet_id
+        new_feeding.save()
+    return redirect('details', pet_id=pet_id)
+
+
+class PetCreate(CreateView):
+    model = Pet
+    fields = '__all__'
+    success_url = '/pets/'
+
+class PetUpdate(UpdateView):
+    model = Pet
+    fields = ['breed', 'description', 'age']
+
+class PetDelete(DeleteView):
+    model = Pet
+    success_url = '/pets/'
